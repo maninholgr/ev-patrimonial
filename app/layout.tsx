930996bd277f84a57e25d246a9335babfa6b63ab
Luciano Goularte da Rosa <114248967+maninholgr@users.noreply.github.com>
2026-05-31 14:29:28 -0300

    Corrige layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Goldusd Investimentos",
   description: "Seu capital em ouro",
-  },
+};
 
 export default function RootLayout({
   children,
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="pt-BR"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
